c3047a88457a44bf470b78802829a2c217f4e05c
sahilsoni20 <ssahil3120@gmail.com>
2024-09-15 20:04:49 +0530

    selecting tools

---
+++ src/components/action-bar/action-bar.tsx
@@ -0,0 +1,40 @@
+import { IoHandRightOutline, IoText } from "react-icons/io5";
+import { Tools, ToolsType } from "../../types";
+import { FiMinus, FiMousePointer, FiSquare } from "react-icons/fi";
+import { LuPencil } from "react-icons/lu";
+import "./style.css";
+
+type ActionBarProps = {
+  tool: ToolsType;
+  setTool: (tool: ToolsType) => void;
+};
+
+export function ActionBar({ tool, setTool }: ActionBarProps) {
+  return (
+    <div className="actionBar">
+      {Object.values(Tools).map((t, index) => (
+        <div
+          className={`inputWrapper ${tool === t ? "selected" : ""}`}
+          key={t}
+          onClick={() => setTool(t)}
+        >
+          <input
+            type="radio"
+            id={t}
+            checked={tool === t}
+            onChange={() => setTool(t)}
+          />
+          <label htmlFor={t}>{t}</label>
+          {t === "pan" && <IoHandRightOutline />}
+          {t === "selection" && <FiMousePointer />}
+          {t === "rectangle" && <FiSquare />}
+          {t === "line" && <FiMinus />}
+          {t === "pencil" && <LuPencil />}
+          {t === "text" && <IoText />}
+
+          <span>{index + 1}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
